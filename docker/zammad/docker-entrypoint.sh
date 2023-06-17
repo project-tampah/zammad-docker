19f3621ce7ac69be0639fd8a0079d14da3fd1cc5
Tobias Lindenberg <tobias@project-tampah.com>
2023-06-17 18:45:18 +0000

    use new searchindex command

--- docker/zammad/docker-entrypoint.sh
+++ docker/zammad/docker-entrypoint.sh
@@ -86,7 +86,7 @@ if [ "$1" = 'zammad-init' ]; then
     if [ "${ELASTICSEARCH_REINDEX}" == "true" ]; then
       if ! curl -s "${SSL_SKIP_VERIFY}" "${ELASTICSEARCH_SCHEMA}://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_cat/indices" | grep -q ${ELASTICSEARCH_NAMESPACE}; then
         echo "rebuilding elasticsearch index..."
-        bundle exec rake searchindex:rebuild
+        bundle exec rake zammad:searchindex:rebuild
       fi
     fi
   fi
